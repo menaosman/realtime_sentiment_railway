03b885d4f143acd21078e15e409f1925bbd849c4
Mena Mohamed <104127946+menaosman@users.noreply.github.com>
2025-05-12 20:23:31 +0300

    Update flask_dashboard.py

--- flask_dashboard.py
+++ flask_dashboard.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string, jsonify
+from flask import Flask, render_template, jsonify
 from pymongo import MongoClient
 import pandas as pd
 import os
@@ -10,7 +10,13 @@ client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)
 collection = client["sentiment_analysis"]["tweets"]
 
 @app.route('/')
-def dashboard():
+def home():
+    data = list(collection.find({}, {"_id": 0}))
+    df = pd.DataFrame(data)
+    return render_template("dashboard.html", tweets=df.to_dict(orient="records"))
+
+@app.route('/api/tweets')
+def api_tweets():
     data = list(collection.find({}, {"_id": 0}))
     return jsonify(data)
 
